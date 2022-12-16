f9a72c68d2ce6bc7d0e354c7e2b0a894fb5f6156
Janet <lis19110494@irapuato.tecnm.mx>
2022-12-16 13:32:44 -0600

    versión con servicios para Usuarios/Miembros

---
+++ Services/model/UsuariosMiembros.php
@@ -0,0 +1,41 @@
+<?php
+
+class UsuariosMiembros extends MethodsCrud {
+
+    public function __construct()
+    {
+        parent::__construct('clubs_itesi');
+    }
+
+    public function get_users_by_club($club_id) {
+        $query = "
+        SELECT miembros_club.id, miembros_club.no_control, miembros_club.nombre,
+        miembros_club.apellido_paterno, miembros_club.apellido_materno, miembros_club.sexo,
+        miembros_club.correo, miembros_club.telefono, miembros_club.rango, miembros_club.semestre,
+        especialidad_miebro.nombre AS especialidad_miembro, rol_miembro_club.nombre AS rol_miembro,
+        clubes.name AS nombre_club
+        FROM miembros_club
+        INNER JOIN especialidad_miebro ON miembros_club.id_especialidad = especialidad_miebro.id
+        INNER JOIN rol_miembro_club ON miembros_club.id_rol_member_club = rol_miembro_club.id
+        INNER JOIN clubes ON miembros_club.id_club= clubes.id
+        WHERE clubes.id = ?;
+        ";
+        
+        $params = array($club_id);
+
+        return $this->select_query($query, $params);
+    }
+
+    public function add_new_user_club($user_info) {
+        $query = "
+        INSERT INTO miembros_club (no_control, nombre, apellido_paterno, apellido_materno, sexo, correo, telefono, rango, semestre, id_especialidad, id_rol_member_club, id_club)";
+        $query .= "VALUES (?,?,?,?,?,?,?,?,?,?,?,?);";
+
+        $data = array($user_info->no_control, $user_info->nombre, $user_info->apellido_paterno, $user_info->apellido_materno, $user_info->sexo, $user_info->correo, $user_info->telefono, $user_info->rango, $user_info->semestre, $user_info->id_especialidad, $user_info->id_rol_member_club, $user_info->id_club);
+
+        return $this->insert_query($query, array($data));
+    }
+
+
+  
+}
