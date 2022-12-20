cb1acbfb7cf7eeeed7f0b42f2433723c68c21155
Janet <lis19110494@irapuato.tecnm.mx>
2022-12-19 19:01:26 -0600

    Versión con servicios para actualizar y borrar evidencias

--- Services/model/Evidences.php
+++ Services/model/Evidences.php
@@ -14,4 +14,16 @@ class Evidences extends MethodsCrud {
 
         return $this->select_query($query, $params);
     }
+
+    public function update_evidence ($evidence_info) {
+        $query = "UPDATE evidencia SET nombre = ?, tipo = ?, ruta = ?, id_actividad = ?
+        WHERE id = ?";
+
+        $data = array($evidence_info->nombre, $evidence_info->tipo, $evidence_info->ruta, $evidence_info->id_actividad,
+                $evidence_info->id);
+        return $this->update_delete_query($query, array($data));
+    }
+
+    
+  
 }
