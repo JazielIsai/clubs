406e3891de67c0e609e5d53cc5acf82dd7574de9
JazielIsai <code.isai.dev@gmail.com>
2023-01-02 00:51:55 -0600

    add component to user permissions

---
+++ src/ClubsAdmin/UserPermissions/UserPermissions.jsx
@@ -0,0 +1,14 @@
+import {useState, useEffect, useContext} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import {AuthContext} from "../../Auth";
+import {useFetch_RequestGet} from "../../hooks/useFetchGet";
+
+export const UserPermissions = () => {
+
+
+    return (
+        <>
+
+        </>
+    )
+}
