eab65bfe740a998d574378bd537c078bb8cea851
JazielIsai <code.isai.dev@gmail.com>
2023-01-11 22:56:37 -0600

    change to a function of type promise

--- src/helpers/Alerts/AlertDialog.js
+++ src/helpers/Alerts/AlertDialog.js
@@ -1,18 +1,19 @@
 import Swal from 'sweetalert2'
 
-export const AlertDialog = (title, message, confirmButtonText, denyButtonText) => {
-    Swal.fire({
+export const AlertDialog = async (title, message, confirmButtonText, denyButtonText) => {
+    return await Swal.fire({
         title: title,
         showDenyButton: true,
-        showCancelButton: true,
         confirmButtonText: confirmButtonText,
         denyButtonText: denyButtonText,
       }).then((result) => {
         /* Read more about isConfirmed, isDenied below */
         if (result.isConfirmed) {
-          Swal.fire('Saved!', '', 'success')
+            Swal.fire('Saved!', '', 'success')
+            return true;
         } else if (result.isDenied) {
-          Swal.fire('Changes are not saved', '', 'info')
+            Swal.fire('Changes are not saved', '', 'info')
+            return false;
         }
       })
 }
