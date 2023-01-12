5e15bf2c1eb9b50e857c7da6ac474042a885f3dc
JazielIsai <code.isai.dev@gmail.com>
2023-01-11 22:59:03 -0600

    change component to used component TableEdit

--- src/ClubsGroup/Members/ViewMembers.jsx
+++ src/ClubsGroup/Members/ViewMembers.jsx
@@ -1,7 +1,10 @@
 import React, {useContext, useEffect, useState} from 'react'
-import { useParams } from 'react-router-dom'
+import {useNavigate, useParams} from 'react-router-dom'
 import { useFetch_RequestGet } from '../../hooks/useFetchGet'
 import {AuthContext} from "../../Auth";
+import {TableEdit} from "../../components/Tables";
+import {useDataCollectionRequest} from "../../hooks/useDataCollectionRequest";
+import {AlertDialog, AlertError, AlertSuccess, AlertToast, requestPost} from "../../helpers";
 
 export const ViewMembers = () => {
 
@@ -9,109 +12,123 @@ export const ViewMembers = () => {
 
     const { user } = useContext(AuthContext);
 
-    const {data} = useFetch_RequestGet('get_users_by_club&club_id='+club_id)
-    
-    const [ getRowMembers, setRowMembers ] = useState();
-    const [getColumnMembers, setColumnMembers] = useState();
+    const navigate = useNavigate();
 
-    useEffect( () => {
+    const { dataCollectionRequest: getUsers  } = useDataCollectionRequest(
+        'get_users_by_club&club_id='+club_id,
+        'all',
+    )
+    const { dataCollectionRequest: getRoles, setDataCollectionRequest  } = useDataCollectionRequest(
+        'get_all_rol_members_clubs',
+        'all',
+    )
 
-        try {
-            setColumnMembers(JSON.parse(data)[0]);
-            setRowMembers(JSON.parse(data));
-        } catch (err ) {
-            console.log(err);
-        }
-        
-    }, [data] )
-
-    const handleNavigateToMember = (id_activity) => {
+    const handleNavigateToMember = (e, dataRow) => {
+        console.log(dataRow);
         if ( user?.id_club == null ) {
-
+            navigate(`/admin/members/edit/${club_id}/${dataRow.id}`);
         } else {
-
+            navigate(`/club/members/edit/${club_id}/${dataRow.id}`);
         }
+
     }
 
-    const handleDelete = (id_activitie) => {
+    const handleUpdateRolMember = (e, dataRow) => {
+        console.log(dataRow);
+
+        const body = {
+            id_rol_member_club: parseInt(e.target.value),
+            id: parseInt(dataRow?.id),
+        }
+
+        console.log("body ->", body);
+
+        const formData = new FormData();
+        formData.append('member_info', JSON.stringify(body));
+
+        requestPost('update_rol_member_club', formData)
+            .then( (response) => {
+                console.log(response.trim());
+                if (response.trim() == '1') {
+                    AlertToast('Rol actualizado correctamente','success',3000);
+                } else {
+                    AlertError('Error', 'No se pudo actualizar el rol del miembro');
+                }
+            } )
+            .catch( (error) => {
+                console.log(error);
+            } );
+
+    }
+
+    const handleDelete = (e, dataRow) => {
         if ( user?.id_club == null ) {
 
         } else {
 
         }
+
+        AlertDialog(`Eliminar`, `¿Deseas eliminar a ${dataRow?.nombre_completo}?`, 'Eliminar', 'Canelar')
+            .then( res => {
+                if (res) {
+                    console.log('el', res)
+                    const formData = new FormData();
+                    formData.append('id_member', dataRow?.id);
+
+                    requestPost('delete_member', formData)
+                        .then( (response) => {
+
+                        } )
+                        .catch( (err) => {
+                            console.log(err);
+                        } )
+                }
+            })
+
     }
 
     return (
-        <div className=''>          
-            <div class="table-responsive table-hover">
-            <table class="table">
-                    <thead>
-                        <tr>
-                            {
-                                getColumnMembers !== null &&
-                                getColumnMembers !== undefined && 
-                                    Object.keys(getColumnMembers).map( (keyRow, index) => {
-                                        
-                                        if (keyRow == 'id') { return null; }
-                                        if (keyRow == 'no_control') {
-                                            keyRow = 'No. Control';
-                                        }
-                                        if (keyRow == 'apellido_paterno') { 
-                                            keyRow = 'Apellido Paterno';
-                                        }
-                                        if (keyRow == 'apellido_materno') {
-                                            keyRow = 'Apellido Materno';
-                                        }
-                                        if (keyRow == 'especialidad_miembro') { return null; }
-                                        if (keyRow == 'nombre_club') { return null; }
+        <div className=''>
 
-                                        keyRow = keyRow.replace(/(^\w{1})|(\s+\w{1})/g, letra => letra.toUpperCase());
+            <TableEdit
+                getColumns={[
+                    {headerName: 'ID', field: 'id', hidden: true},
+                    {headerName: 'No. Control', field: 'no_control'},
+                    {headerName: 'Nombre', field: 'nombre_completo'},
+                    {
+                        headerName: 'Rol',
+                        field: 'id_rol_member_club',
+                        type: 'select',
+                        options: getRoles && getRoles.map ( (rol) => {
+                            return { value: rol.id, label: rol.nombre }
+                        } ),
+                        onChange: handleUpdateRolMember,
 
-                                        return(
-                                            <th key={index} scope="col"> {keyRow} </th>
-                                        )
-                                    })
-                                    
-                            }
-                            <th scope="col"> Ir a </th>
+                    },
+                    {headerName: 'Correo', field: 'correo'},
+                    {headerName: 'Telefono', field: 'telefono'},
+                    {headerName: 'Especialidad', field: 'especialidad_miembro'},
+                    {
+                        headerName: 'Editar',
+                        field: 'editar',
+                        type: 'button',
+                        onClick: handleNavigateToMember,
+                        nameClass: 'btn btn-primary',
+                        label: 'Editar'
+                    },
+                    {
+                        headerName: 'Eliminar',
+                        field: 'eliminar',
+                        type: 'button',
+                        onClick: handleDelete,
+                        nameClass: 'btn btn-danger',
+                        label: 'Eliminar'
+                    }
 
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            getRowMembers !== null &&
-                            getRowMembers !== undefined && 
-                                getRowMembers.map( (activity, index) => (
-                                    <tr key={index}>
+                ]}
+                getRows={getUsers && getRoles ? getUsers : []}
+            />
 
-                                        {/* <th scope="row"> {club?.id_club} </th> */}
-                                        <td> {activity?.no_control} </td>
-                                        <td> { activity?.nombre } </td>
-                                        <td>  {activity?.apellido_paterno} </td>
-                                        <td>  { activity?.apellido_materno }   </td>
-                                        <td>  {activity?.sexo} </td>
-                                        <td>  {activity?.correo} </td>
-                                        <td>  {activity?.telefono} </td>
-                                        <td>  {activity?.rango} </td>
-                                        <td>  {activity?.semestre} </td>
-                                        {/* <td>  {activitie?.especialidad_miembro} </td> */}
-
-                                        <td>  {activity?.rol_miembro} </td>
-                                        {/* <td>  {activitie?.nombre_club} </td> */}
-
-                                        <td>
-                                            <button onClick={()=>handleNavigateToMember(activity?.id)} class="btn btn-primary"> Editar </button>
-                                        </td>
-                                        <td>
-                                            <button onClick={()=>handleDelete(activity?.id)} className="btn btn-danger"> Eliminar </button>
-                                        </td>
-
-                                    </tr>
-                                ))
-                        }
-                    </tbody>
-                </table>
-            </div>
         </div>
     )
 }
