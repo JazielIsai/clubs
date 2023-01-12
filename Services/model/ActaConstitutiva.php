1c5c7a1084d2c426f1e5afd5a9657f6443cf9300
JazielIsai <code.isai.dev@gmail.com>
2023-01-12 08:42:27 -0600

    add method to upload file

--- Services/model/ActaConstitutiva.php
+++ Services/model/ActaConstitutiva.php
@@ -1,8 +1,13 @@
 <?php
 
-class ActaConstitutiva extends MethodsCrud{
+class ActaConstitutiva extends MethodsCrud {
+
+    private $uploadDocument;
+
     public function __construct(){
         parent:: __construct('clubs_itesi');
+
+        $this->uploadDocument = new UploadDocument();
     }
 
     public function get_all_acta () {
@@ -13,13 +18,32 @@ class ActaConstitutiva extends MethodsCrud{
         return $this->select_query($query);
     }
 
-    public function add_new_acta ($acta_info) {
+    public function add_new_acta ($acta_info, $nameClub) {
 
-        $query = "
-                INSERT INTO clubs_itesi.archivos_acta_constitutiva_club(nombre, ruta, id_club) VALUES (?, ?, ?);
-                ";
-        $params = array($acta_info->nombre, $acta_info->ruta, $acta_info->id_club);
-        return $this->insert_query($query, array($params));
+        $path_destination = './clubs/' . $acta_info->id_club . '_' . $nameClub . '/plan_anual/' ;
+
+        $response = $this->uploadDocument->upload_file($_FILES['file_info'], $path_destination, 240000);
+
+        if ($response['upload']) {
+
+            $query = "
+                        INSERT INTO clubs_itesi.archivos_acta_constitutiva_club (nombre, ruta, id_club) 
+                        VALUES (?, ?, ?);
+                     ";
+
+            $params = array(
+                $acta_info->nombre,
+                $response['file_destination'],
+                $acta_info->id_club
+            );
+
+            return $this->insert_query($query, array($params));
+
+        } else {
+            return $response;
+        }
+
+
     }
 
 
