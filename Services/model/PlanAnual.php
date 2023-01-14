9f87b69f2dfa23d669b944aa678118273e443cb9
JazielIsai <code.isai.dev@gmail.com>
2023-01-13 20:36:02 -0600

    update method to upload plan anual by club

--- Services/model/PlanAnual.php
+++ Services/model/PlanAnual.php
@@ -17,19 +17,34 @@ class PlanAnual extends MethodsCrud {
         return $this->select_query($query);
     }
 
+    public function get_plan_anual_by_club_id ($id_club) {
+        $query = "
+                    SELECT * FROM clubs_itesi.archivos_plan_anual_club WHERE id_club = ?
+                ";
+
+        $params = array($id_club);
+
+        return $this->select_query($query, $params);
+    }
+
     public function add_new_planAnual ($plan_info, $nameClub) {
 
-        $path_destination = './clubs/' . $plan_info->id_club . '_' . $nameClub . '/plan_anual/' ;
+        $path_destination = './clubs_files/' . $plan_info->id_club . '_' . $nameClub . '/plan_anual/' ;
 
-        $response = $this->uploadDocument->upload_file($_FILES['file_info'], $path_destination, 240000);
+        $response = $this->uploadDocument->upload_file($_FILES['file_plan_anual'], $path_destination, 2097152);
 
         if ($response['upload']) {
-            $query = "INSERT INTO archivos_plan_anual_club (nombre, ruta, id_club) VALUES (?, ?, ?)";
-            $data = array(
+            $query = "
+                        INSERT INTO archivos_plan_anual_club (nombre, ruta, id_club) 
+                        VALUES (?, ?, ?);
+                     ";
+
+            $data = array (
                 $plan_info->nombre,
                 $response['file_destination'],
                 $plan_info->id_club
             );
+
             return $this->insert_query($query, array($data));
         } else {
             return $response;
