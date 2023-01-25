57720c6e0bfcd89bd4e1b56784464972ccff22da
JazielIsai <code.isai.dev@gmail.com>
2023-01-24 21:57:41 -0600

    add components to view evidence by activity component

--- src/ClubsGroup/Activities/Evidences/ViewEvidenceByActivity.jsx
+++ src/ClubsGroup/Activities/Evidences/ViewEvidenceByActivity.jsx
@@ -1,32 +1,56 @@
 import React, {useState, useEffect, useContext} from "react";
 import {useNavigate, useParams} from "react-router-dom";
 import {AuthContext} from "../../../Auth";
-import {useFetch_RequestGet} from "../../../hooks/useFetchGet";
-import {TableEvidences} from "./TableEvidences";
+import {useDataCollectionRequest} from "../../../hooks/useDataCollectionRequest";
+import { Layout, Model } from "flexlayout-react";
+import {layoutEvidenceByActivity} from "./Layout/layoutEvidenceByActivity";
+import {FoldersEvidencies} from "./components/FoldersEvidencies";
+import {ViewEvidencies} from "./components/ViewEvidencie";
 
 export const ViewEvidenceByActivity = () => {
 
-    const navigate = useNavigate();
-    const { id_activitie, nameActivitie } = useParams();
+    const { activity_id, activity_name } = useParams();
 
+    const navigate = useNavigate();
+
+    const [model] = useState(Model.fromJson(layoutEvidenceByActivity));
+
+    const { dataCollectionRequest : getActivity } = useDataCollectionRequest(
+        '',
+        'row'
+    )
+
+    const factory = (node, TabNode) => {
+        let component = node.getComponent();
+
+        if (component === "menu") {
+            return (
+                <div className='container'>
+                    <h1 className={' fs-4 text-center mt-1 '}>
+                        {activity_name}
+                    </h1>
+                </div>
+            )
+
+        } else if (component === "folders") {
+            return (
+                <FoldersEvidencies />
+            )
+        } else if (component === "evidences") {
+            return (
+                <ViewEvidencies />
+            )
+        }
+
+    }
 
 
     return (
-        <>
-            <div className='row'>
-
-                <div className='col-12 col-md-6'>
-
-
-                </div>
-
-                <div className='col-12 col-md-6' >
-
-                    <TableEvidences id_activity={id_activitie} />
-
-                </div>
-
-            </div>
-        </>
+        <div className={'evidencie_by_activity'}>
+            <Layout
+                model={model}
+                factory={factory}
+            />
+        </div>
     )
 }
