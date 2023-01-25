5bc479722348a59dacbd04868f6be1295b47cf57
JazielIsai <code.isai.dev@gmail.com>
2023-01-24 21:54:21 -0600

    add or upload routes

--- src/ClubsAdmin/Routes/RoutesClubs.js
+++ src/ClubsAdmin/Routes/RoutesClubs.js
@@ -20,6 +20,11 @@ import { AdminGeneral } from '../AdminGeneral/AdminGeneral';
 import { Members } from '../../ClubsGroup/Members/Members';
 import { AddMember } from '../../ClubsGroup/Members/AddMember';
 import { EditMember } from '../../ClubsGroup/Members/EditMember';
+import {EvaluatePerformance} from "../../ClubsGroup/Activities/EvaluatePerformance/EvaluatePerformance";
+import {ViewEvidences} from "../../ClubsGroup/Activities/Evidences/ViewEvidences";
+import {ViewEvidenceByActivity} from "../../ClubsGroup/Activities/Evidences/ViewEvidenceByActivity";
+import {Events} from "../../ClubsGroup/Activities/Events/Events";
+import {Reports} from "../../ClubsGroup/Reports/Reports";
 
 
 export const RoutesClubs = () => {
@@ -32,23 +37,32 @@ export const RoutesClubs = () => {
               <Route path="dashboard" element={<Dashboard />} />
 
               <Route path='registerClub' element={<RegisterClub />} />
-              <Route path='updateClub' element={<EditClub />} />
+              <Route path='updateClub/:club_id' element={<EditClub />} />
               
               <Route path="viewClubs" element={<ViewsClubs />} />
               <Route path="viewClubs/:id/:club_name" element={<DashboardByClub />} />
               <Route path="registerClub" element={<RegisterClub />} />
               
-              <Route path="activities/:club_id" element={<Activities />} />
+              <Route path="activities/:club_id/:club_name" element={<Activities />} />
               <Route path='activities/add/:club_id' element={<NewActivite />} />
               <Route path='activities/edit/:club_id/:id_activitie' element={<EditActivitie/>} />
-              
-              <Route path='activities/evidences/:id_activitie/:nameActivitie' element={<Evidences />} />
+
+              <Route path='evidences/:club_id/' element={<ViewEvidences />} />
+              <Route path='activities/evidences/:club_id/:name_club/:id_activitie/:nameActivitie' element={<Evidences />} />
               <Route path='activities/evidences/add/:id_activitie' element={<AddEvidences />} />
               <Route path='activities/evidences/edit/:club_id/:id_evidence' element={<EditEvidences />} />
+              <Route path={'activities/view_evidencie_by_activity/:activity_id/:activity_name'} element={<ViewEvidenceByActivity />} />
+
+              <Route path='activities/evaluateMember/:club_id/:idActivity/:nameActivity' element={<EvaluatePerformance /> } />
+
 
               <Route path='members/:club_id' element={<Members />} />
               <Route path='members/add/:club_id' element={<AddMember />} />
               <Route path='members/edit/:club_id/:id_member' element={<EditMember />} />
+
+              <Route path='events_public' element={<Events />} />
+
+              <Route path={'reports_by_club/:club_id'} element={<Reports />} />
 
               <Route path="viewUsers" element={<ViewUsers />} />
               <Route path='registerUsers' element={<RegisterUser />} />
