8e95e63a124314066badba04b889048c1ffa085b
JazielIsai <code.isai.dev@gmail.com>
2023-02-16 15:50:49 -0600

    controller the view preview when the file is open and the user back to other page then file close

--- src/ClubsGroup/Activities/Evidences/components/ViewPDF.jsx
+++ src/ClubsGroup/Activities/Evidences/components/ViewPDF.jsx
@@ -1,18 +1,30 @@
+import {useEffect} from "react";
 
 export const ViewPDF = ( { pathFile = '' } ) => {
+
+
+    useEffect(() => {
+
+        const pdfHolder = document.getElementById('pdfHolder');
+        pdfHolder.data = pathFile;
+
+        return () => {
+            pdfHolder.data = '';
+            pathFile = '';
+        }
+
+    } , [pathFile])
 
 
     return (
         <div className={'w-100 h-100 d-flex flex-column'}>
             <object
                 id={"pdfHolder"}
-                data={pathFile}
                 type="application/pdf"
                 width="100%"
                 height="100%"
 
             >
-                <embed id={"pdfHolder"} src={pathFile} type="application/pdf" />
             </object>
         </div>
     )
