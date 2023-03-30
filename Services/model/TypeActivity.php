22a36cd707c7a3df0c7ee6bb96c3aac57067beae
l-gonzo <9o.mndez@gmail.com>
2023-03-30 11:01:40 -0600

    services for type_activity

--- Services/model/TypeActivity.php
+++ Services/model/TypeActivity.php
@@ -15,4 +15,34 @@ class TypeActivity extends MethodsCrud {
         return $this->select_query($query);
     }
 
+    public function add_type_activity($name_activity)
+    {
+        echo "name".$name_activity;
+
+        $query = "
+            INSERT INTO tipo_actividad(nombre) VALUES (?);
+            ";
+        return $this->insert_query($query, array(array($name_activity)));
+    }
+
+    public function delete_type_activity($id_type_activity)
+    {
+        $query = "DELETE FROM tipo_actividad WHERE id = ?";
+
+        $data = array($id_type_activity);
+
+        return $this->update_delete_query($query, array($data));
+    }
+
+    public function update_type_activity($activity_info)
+    {
+        $query = "
+            UPDATE tipo_actividad SET nombre = ?
+            WHERE id = ?";
+
+        $data = array($activity_info->name,
+                      $activity_info->id);
+        return $this->update_delete_query($query, array($data));
+    }
+
 }
--- Services/services.php
+++ Services/services.php
@@ -352,6 +352,20 @@ switch ($servicesName){
             echo $services_type_activity->add_type_activity($_POST['name_type_activity']);
         }
         break;
+    case 'delete_type_activity':
+        if (!isset($_POST['id_type_activity'])){
+            echo 'Error: missing info.';
+        } else {
+            echo $services_type_activity->delete_type_activity($_POST['id_type_activity']);
+        }
+        break;
+    case 'update_type_activity':
+        if (!isset($_POST['name_type_activity'], $_POST['id_type_activity'])){
+            echo 'Error: missing info.';
+        } else {
+            echo $services_type_activity->update_type_activity($_POST['name_type_activity'],  $_POST['id_type_activity']);
+        }
+        break;
 
 
 //Skills
