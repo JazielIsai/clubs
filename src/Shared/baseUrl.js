2908a11216c866c48323bd31be4d522cac8b1a33
Jaziel Isaí <92839539+JazielIsai@users.noreply.github.com>
2024-12-19 12:08:26 -0600

    cambios en las configuraciones

--- src/Shared/baseUrl.js
+++ src/Shared/baseUrl.js
@@ -1,2 +1,4 @@
-export const urlDBLogin = 'http://localhost/clubs/Services/';
-export const urlDB = 'http://localhost/clubs/Services/services.php?servicesName=';
+// export const urlDBLogin = 'http://localhost/clubs/Services/';
+// export const urlDB = 'http://localhost/clubs/Services/services.php?servicesName=';
+export const urlDBLogin = 'https://clubs.isai-dev.com/Services/';
+export const urlDB = 'https://clubs.isai-dev.com/Services/services.php?servicesName=';
